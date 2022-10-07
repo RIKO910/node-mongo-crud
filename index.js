157ce7c8279bb479d8faa213c4215425dd9b8a45
RIKO910 <tarikulislamriko910@gmail.com>
2022-10-07 23:31:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express =require('express');
+const app =express();
+const cors =require('cors')
+const port =process.env.PORT||5000;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+//use middleware
+app.use(cors());
+app.use(express.json());
+
+//user:rikouser
+//password:zsIaHntenATorwzs
+
+
+const uri = "mongodb+srv://rikouser:zsIaHntenATorwzs@cluster0.fc3dngw.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        await client.connect();
+        const userCollection =client.db("foodExpress").collection("users");
+
+        //load data
+        app.get('/user',async(req,res)=>{
+            const query ={};
+            const cursor =userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users)
+        })
+        //post user :add a new user
+        app.post('/user',async(req,res)=>{
+            const newUser =req.body
+            console.log('adding a new user',newUser);
+            const result =await userCollection.insertOne(newUser);
+            res.send(result)
+        })
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+
+run().catch(console.dir)
+
+
+app.get('/',(req,res)=>{
+    res.send('server is running okk vaiya')
+})
+
+app.listen(port,()=>{
+    console.log('CRUD server is running');
+})
